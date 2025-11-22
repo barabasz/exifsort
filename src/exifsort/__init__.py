d2c487474d2c4608d504fd81a14a3734cf0b701f
Andrzej Barabasz <abarabasz@gmail.com>
2025-11-22 20:48:25 +0100

    fix

--- src/exifsort/__init__.py
+++ src/exifsort/__init__.py
@@ -5,7 +5,7 @@ ExifSort is a modern Python library for managing media file organization
 based on their EXIF metadata.
 """
 
-__all__ = ["TyConf"]
+__all__ = ["ExifSort"]
 __version__ = "0.2.0"
 __date__ = "2025-01-22"
 __author__ = "barabasz"
